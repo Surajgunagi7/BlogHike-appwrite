d2fbdb10e09ec99f8e3b9b5a3551515970cedd4b
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-08-10 13:34:57 +0530

    config - tailwind | Store | test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-
+/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css'
+import { useState, useEffect } from 'react'
+import authService from "./appwrite/auth"
+import {useDispatch } from "react-redux"
+import {login, logout} from './store/authSlice'
+import {Header,Footer} from './components'
 
 function App() {
-    
-  return (
-    <>
-      <h1>Hello world With</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentAccount()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally( () => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block '>
+        <Header />
+          <main>
+            {/* <Outlet> */}
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+  
 }
 
 export default App
