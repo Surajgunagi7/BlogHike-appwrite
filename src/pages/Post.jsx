ed4c0bfd8d472b4b90db63c54d42bec3a21177c4
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-08-15 22:37:47 +0530

    fixed the home.jsx

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -13,9 +13,7 @@ export default function Post() {
 
     const isAuthor = post && userData ? post.userId === userData.$id : false;
 
-    useEffect(() => {
-        console.log("post: ",post);
-        
+    useEffect(() => {        
         if(slug) {
             service.getPost(slug).then((post) => {
                 if(post) setPost(post)
